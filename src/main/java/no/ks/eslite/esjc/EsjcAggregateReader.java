8d88a3246a7c36ef415275acd09ef4d25924dca5
Idar Borlaug <idar.borlaug@gmail.com>
2018-03-21 12:20:25 +0100

    Error

--- src/main/java/no/ks/eslite/esjc/EsjcAggregateReader.java
+++ src/main/java/no/ks/eslite/esjc/EsjcAggregateReader.java
@@ -5,6 +5,8 @@ import no.ks.eslite.framework.Aggregate;
 import no.ks.eslite.framework.AggregateReader;
 import no.ks.eslite.framework.EventDeserializer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class EsjcAggregateReader implements AggregateReader {
@@ -20,16 +22,18 @@ public class EsjcAggregateReader implements AggregateReader {
     }
 
     @Override
-    public Aggregate read(UUID aggregateId, Aggregate aggregate) {
+    public Aggregate read(UUID aggregateId,final Aggregate aggregate) {
+        final List<Aggregate> mutatedaggregate= new ArrayList<>(1);
         try {
+            mutatedaggregate.add(0, aggregate);
             eventStore.streamEventsForward(this.esjcStreamIdGenerator.generateStreamId(aggregate.getAggregateType(), aggregateId), 0, 100, true)
-                    .forEach(p -> aggregate.apply(aggregate, deserializer.deserialize(p.event.data, p.event.eventType), p.event.eventNumber));
+                    .forEach(p -> mutatedaggregate.add(0,mutatedaggregate.get(0).apply(mutatedaggregate.get(0), deserializer.deserialize(p.event.data, p.event.eventType), p.event.eventNumber)));
         } catch(IllegalStateException e){
             if("Unexpected read status: StreamNotFound".equals(e.getMessage())){
                 return aggregate.withCurrentEventNumber(-1);
             }
             throw e;
         }
-        return aggregate;
+        return mutatedaggregate.get(0);
     }
 }
