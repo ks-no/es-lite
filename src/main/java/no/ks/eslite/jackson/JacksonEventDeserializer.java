095e9a0fbd6df8229c86e7f94756eb0402902842
Andreas Berre <andreas.berre@webstep.no>
2018-04-11 10:07:01 +0200

    verify that events have been annotated, and avoid exposing vavr collections

--- src/main/java/no/ks/eslite/jackson/JacksonEventDeserializer.java
+++ src/main/java/no/ks/eslite/jackson/JacksonEventDeserializer.java
@@ -10,21 +10,24 @@ import no.ks.eslite.framework.EventDeserializer;
 import no.ks.eslite.framework.EventType;
 
 import java.io.IOException;
-import java.util.Set;
 
 public class JacksonEventDeserializer implements EventDeserializer {
 
     private final ObjectMapper objectMapper;
     private final Map<String, Class<? extends Event>> events;
 
-    public JacksonEventDeserializer(ObjectMapper objectMapper, Set<Class<? extends Event>> events) {
-        this.objectMapper = objectMapper;
-        this.events = HashSet.ofAll(events)
-                .toMap(API.unchecked(p -> Tuple.of(p.getAnnotation(EventType.class).value(), p)));
+    public JacksonEventDeserializer(java.util.Set<Class<? extends Event>> events) {
+        this(new ObjectMapper(), events);
     }
 
-    public JacksonEventDeserializer(Set<Class<? extends Event>> events) {
-        this(new ObjectMapper(), events);
+    public JacksonEventDeserializer(ObjectMapper objectMapper, java.util.Set<Class<? extends Event>> events) {
+        this.objectMapper = objectMapper;
+        this.events = HashSet.ofAll(events)
+                .peek(p -> {
+                    if (p.isAnnotationPresent(EventType.class))
+                        throw new RuntimeException(String.format("The event-class \"%s\" has not been annotated with the EventType annotation", p.getSimpleName()));
+                })
+                .toMap(API.unchecked(p -> Tuple.of(p.getAnnotation(EventType.class).value(), p)));
     }
 
     @Override
