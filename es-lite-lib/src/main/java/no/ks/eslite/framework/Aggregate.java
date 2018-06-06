13382c9763dfe4f56e665ad6cd0d5de4a4e22400
Andreas Berre <andreas.berre@webstep.no>
2018-06-06 08:18:21 +0200

    updating aggregate state should not be mandatory

--- es-lite-lib/src/main/java/no/ks/eslite/framework/Aggregate.java
+++ es-lite-lib/src/main/java/no/ks/eslite/framework/Aggregate.java
@@ -5,35 +5,33 @@ import io.vavr.collection.Map;
 
 import java.util.function.BiFunction;
 
-import static java.lang.String.format;
-
 public abstract class Aggregate<AGGREGATE extends Aggregate, EVENT_TYPE extends Event> {
 
     private Map<Class<? extends Event>, BiFunction<Aggregate, Event, Aggregate>> applicators = HashMap.empty();
     private long currentEventNumber;
 
     @SuppressWarnings("unchecked")
-    protected <E extends EVENT_TYPE> void onEvent(Class<E> eventClass, BiFunction<AGGREGATE, E, AGGREGATE> applicatorFunction){
+    protected <E extends EVENT_TYPE> void onEvent(Class<E> eventClass, BiFunction<AGGREGATE, E, AGGREGATE> applicatorFunction) {
         applicators = applicators.put(eventClass, (BiFunction<Aggregate, Event, Aggregate>) applicatorFunction);
     }
 
     public abstract String getAggregateType();
 
-    public long getCurrentEventNumber(){
+    public long getCurrentEventNumber() {
         return currentEventNumber;
     }
 
     @SuppressWarnings("unchecked")
-    public AGGREGATE withCurrentEventNumber(long currentEventNumber){
+    public AGGREGATE withCurrentEventNumber(long currentEventNumber) {
         this.currentEventNumber = currentEventNumber;
         return (AGGREGATE) this;
-    };
+    }
 
-    public Aggregate apply(Aggregate aggregate, Event event, long eventNumber){
+    public Aggregate apply(Aggregate aggregate, Event event, long eventNumber) {
         this.currentEventNumber = eventNumber;
+
         return applicators
-                .get(event.getClass())
-                .getOrElseThrow(() -> new RuntimeException(format("No applicator found for event %s", event.getClass().getSimpleName())))
+                .getOrElse(event.getClass(), (a, e) -> a)
                 .apply(aggregate, event)
                 .withCurrentEventNumber(currentEventNumber);
     }
