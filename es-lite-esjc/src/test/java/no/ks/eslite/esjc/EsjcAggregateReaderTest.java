c46cf524f5b38f26cd997969d70174403de85bab
Andreas Berre <andreas.berre@webstep.no>
2019-10-15 07:17:11 +0200

    added a unit test to confirm

--- es-lite-esjc/src/test/java/no/ks/eslite/esjc/EsjcAggregateReaderTest.java
+++ es-lite-esjc/src/test/java/no/ks/eslite/esjc/EsjcAggregateReaderTest.java
@@ -2,6 +2,7 @@ package no.ks.eslite.esjc;
 
 import com.github.msemys.esjc.EventStore;
 import com.github.msemys.esjc.ResolvedEvent;
+import com.github.msemys.esjc.operation.StreamNotFoundException;
 import com.github.msemys.esjc.proto.EventStoreClientMessages;
 import com.google.protobuf.ByteString;
 import no.ks.eslite.esjc.jackson.JacksonEventSerdes;
@@ -26,7 +27,7 @@ class EsjcAggregateReaderTest {
 
     @Test
     @DisplayName("Test that the reader retrieves incoming events from the event-store")
-    void name() throws UnsupportedEncodingException {
+    void testReadEvents() throws UnsupportedEncodingException {
         JacksonEventSerdes eventSerdes = new JacksonEventSerdes(singleton(HiredEvent.class));
 
         EventStore eventStoreMock = mock(EventStore.class);
@@ -54,6 +55,21 @@ class EsjcAggregateReaderTest {
         assertNotNull(aggregate);
         assertEquals(aggregateId, aggregate.getAggregateId());
         assertEquals(startDate, aggregate.getStartDate());
+    }
 
+    @Test
+    @DisplayName("Test that the reader returns an empty aggregate if no stream is found")
+    void testReadEmptyAggregate() throws UnsupportedEncodingException {
+        JacksonEventSerdes eventSerdes = new JacksonEventSerdes(singleton(HiredEvent.class));
+
+        EventStore eventStoreMock = mock(EventStore.class);
+
+        when(eventStoreMock.streamEventsForward(anyString(), anyLong(), anyInt(), anyBoolean()))
+                .thenThrow(new StreamNotFoundException("some message"));
+
+        EsjcAggregateReader reader = new EsjcAggregateReader(eventStoreMock, eventSerdes, (t, id) -> t + "." + id);
+        Employee aggregate = (Employee) reader.read(UUID.randomUUID(), Employee.builder().build());
+        assertNotNull(aggregate);
+        assertEquals(-1, aggregate.getCurrentEventNumber());
     }
 }
